3efec8c6d4615236ef7962e2a43e560034211608
anasfaisalamri <anasfaisalamri@gmail.com>
2024-07-02 17:32:01 +0700

    ubah tanggal dan jam

--- assets/js/countdown.js
+++ assets/js/countdown.js
@@ -2,7 +2,7 @@ simplyCountdown(".simply-countdown", {
   year: 2024, // required
   month: 7, // required
   day: 7, // required
-  hours: 8, // Default is 0 [0-23] integer
+  hours: 19, // Default is 0 [0-23] integer
   words: {
     //words displayed into the countdown
     days: { singular: "hari", plural: "hari" },
